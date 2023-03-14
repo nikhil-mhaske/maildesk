b9d252527f7c8d3274a2a229bce0d3863409dd95
wisdm-nikhil <nikhil.mhaske@wisdmlabs.com>
2023-03-14 13:19:31 +0530

    Mail Sent on every minute. Using System Cron

--- maildesk.php
+++ maildesk.php
@@ -5,7 +5,32 @@
 * Author: Nikhil Mhaske
 * Author URI: https://nikhil.wisdmlabs.net
 * Description: Posts summary on Admin Mail at End of the Day
+* Text Domain: maildesk
 */
+
+add_action( 'wp', 'schedule_daily_post_summary' );
+
+
+if( ! function_exists( 'md_add_cron_schedules' ) ) :
+    function md_add_cron_schedules( $schedules = array() ) {
+    
+    $schedules['every_minute'] = array(
+    'interval' => 60,
+    'display' => __( 'Every Minute', 'maildesk' ),
+    );
+    return $schedules;
+    }
+    add_filter( 'cron_schedules', 'md_add_cron_schedules' );
+endif;
+
+
+function schedule_daily_post_summary() {
+    if ( ! wp_next_scheduled( 'send_daily_post_summary' ) ) {
+        wp_schedule_event( time(), 'every_minute', 'send_daily_post_summary' );
+    }
+}
+
+add_action( 'send_daily_post_summary', 'send_daily_post_summary_callback' );
 
 
 function get_daily_post_summary() {
@@ -24,6 +49,9 @@ function get_daily_post_summary() {
         $post_data = array(
             'title' => $post->post_title,
             'url' => get_permalink( $post->ID ),
+            'meta_title' => get_post_meta( $post->ID, '_yoast_wpseo_title', true ),
+            'meta_description' => get_post_meta( $post->ID, '_yoast_wpseo_metadesc', true ),
+            'meta_keywords' => get_post_meta( $post->ID, '_yoast_wpseo_focuskw', true ),
         );
         array_push( $summary, $post_data );
     }
@@ -32,18 +60,24 @@ function get_daily_post_summary() {
 }
 
 
-function send_daily_post_summary() {
+function send_daily_post_summary_callback() {
     $to = get_option( 'admin_email' );
     $subject = 'Daily Post Summary';
     $summary = get_daily_post_summary();
     $message = '';
+
     foreach ( $summary as $post_data ) {
         $message .= 'Title: ' . $post_data['title'] . "\n";
         $message .= 'URL: ' . $post_data['url'] . "\n";
+        $message .= 'Meta Title: ' . $post_data['meta_title'] . "\n";
+        $message .= 'Meta Description: ' . $post_data['meta_description'] . "\n";
+        $message .= 'Meta Keywords: ' . $post_data['meta_keywords'] . "\n";
+        $message .= "\n";
     }
-    wp_mail( $to, $subject, $message );
-}
+    $headers = 'Content-Type: text/html; charset=UTF-8';
 
-add_action( 'publish_post', 'send_daily_post_summary' );
+    mail( $to , $subject, $message , $headers);
+    
+}
 
 ?>
