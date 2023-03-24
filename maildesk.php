2683a404d9181cd80d13c3837e2402f4e843b9d7
wisdm-nikhil <nikhil.mhaske@wisdmlabs.com>
2023-03-24 12:44:25 +0530

    WebPageTest API Bug Fixed

--- maildesk.php
+++ maildesk.php
@@ -26,8 +26,8 @@
  */
 
 // If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) {
-	die;
+if (!defined('WPINC')) {
+    die;
 }
 
 /**
@@ -35,34 +35,36 @@ if ( ! defined( 'WPINC' ) ) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define( 'MAILDESK_VERSION', '1.0.0' );
+define('MAILDESK_VERSION', '1.0.0');
 
 /**
  * The code that runs during plugin activation.
  * This action is documented in includes/class-maildesk-activator.php
  */
-function activate_maildesk() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-maildesk-activator.php';
-	Maildesk_Activator::activate();
+function activate_maildesk()
+{
+    require_once plugin_dir_path(__FILE__) . 'includes/class-maildesk-activator.php';
+    Maildesk_Activator::activate();
 }
 
 /**
  * The code that runs during plugin deactivation.
  * This action is documented in includes/class-maildesk-deactivator.php
  */
-function deactivate_maildesk() {
-	require_once plugin_dir_path( __FILE__ ) . 'includes/class-maildesk-deactivator.php';
-	Maildesk_Deactivator::deactivate();
+function deactivate_maildesk()
+{
+    require_once plugin_dir_path(__FILE__) . 'includes/class-maildesk-deactivator.php';
+    Maildesk_Deactivator::deactivate();
 }
 
-register_activation_hook( __FILE__, 'activate_maildesk' );
-register_deactivation_hook( __FILE__, 'deactivate_maildesk' );
+register_activation_hook(__FILE__, 'activate_maildesk');
+register_deactivation_hook(__FILE__, 'deactivate_maildesk');
 
 /**
  * The core plugin class that is used to define internationalization,
  * admin-specific hooks, and public-facing site hooks.
  */
-require plugin_dir_path( __FILE__ ) . 'includes/class-maildesk.php';
+require plugin_dir_path(__FILE__) . 'includes/class-maildesk.php';
 
 /**
  * Begins execution of the plugin.
@@ -73,42 +75,45 @@ require plugin_dir_path( __FILE__ ) . 'includes/class-maildesk.php';
  *
  * @since    1.0.0
  */
-function run_maildesk() {
+function run_maildesk()
+{
 
-	$plugin = new Maildesk();
-	$plugin->run();
-
+    $plugin = new Maildesk();
+    $plugin->run();
 }
 run_maildesk();
 
 //Code Starts Here
 
-add_action( 'wp', 'schedule_daily_post_summary' );
+add_action('wp', 'schedule_daily_post_summary');
 
 //Made for Trial of Sending Mail Every Minute
-if( ! function_exists( 'md_add_cron_schedules' ) ) :
-    function md_add_cron_schedules( $schedules = array() ) {
-    
-    $schedules['every_minute'] = array(
-    'interval' => 120,
-    'display' => __( 'Every Minute', 'maildesk' ),
-    );
-    return $schedules;
+if (!function_exists('md_add_cron_schedules')) :
+    function md_add_cron_schedules($schedules = array())
+    {
+
+        $schedules['every_minute'] = array(
+            'interval' => 60,
+            'display' => __('Every Minute', 'maildesk'),
+        );
+        return $schedules;
     }
-    add_filter( 'cron_schedules', 'md_add_cron_schedules' );
+    add_filter('cron_schedules', 'md_add_cron_schedules');
 endif;
 
 
-function schedule_daily_post_summary() {
-    if ( ! wp_next_scheduled( 'send_daily_post_summary' ) ) {
-        wp_schedule_event( time(), 'daily', 'send_daily_post_summary' );
+function schedule_daily_post_summary()
+{
+    if (!wp_next_scheduled('send_daily_post_summary')) {
+        wp_schedule_event(time(), 'daily', 'send_daily_post_summary');
     }
 }
 
-add_action( 'send_daily_post_summary', 'send_daily_post_summary_callback' );
+add_action('send_daily_post_summary', 'send_daily_post_summary_callback');
 
 
-function get_daily_post_summary() {
+function get_daily_post_summary()
+{
     $args = array(
         'date_query' => array(
             array(
@@ -116,33 +121,34 @@ function get_daily_post_summary() {
             ),
         ),
     );
-    $query = new WP_Query( $args );
+    $query = new WP_Query($args);
     $posts = $query->posts;
     $summary = array();
 
-    foreach ( $posts as $post ) {
+    foreach ($posts as $post) {
         $post_data = array(
             'title' => $post->post_title,
-            'url' => get_permalink( $post->ID ),
-            'meta_title' => get_post_meta( $post->ID, '_yoast_wpseo_title', true ),
-            'meta_description' => get_post_meta( $post->ID, '_yoast_wpseo_metadesc', true ),
-            'meta_keywords' => get_post_meta( $post->ID, '_yoast_wpseo_focuskw', true ),
-            'page_speed' => get_page_speed_score( get_permalink( $post->ID ) ),
+            'url' => get_permalink($post->ID),
+            'meta_title' => get_post_meta($post->ID, '_yoast_wpseo_title', true),
+            'meta_description' => get_post_meta($post->ID, '_yoast_wpseo_metadesc', true),
+            'meta_keywords' => get_post_meta($post->ID, '_yoast_wpseo_focuskw', true),
+            'page_speed' => get_page_speed_score(get_permalink($post->ID)),
         );
-        array_push( $summary, $post_data );
+        array_push($summary, $post_data);
     }
 
     return $summary;
 }
 
 
-function send_daily_post_summary_callback() {
-    $to = get_option( 'admin_email' );
+function send_daily_post_summary_callback()
+{
+    $to = get_option('admin_email');
     $subject = 'Daily Post Summary';
     $summary = get_daily_post_summary();
     $message = '';
 
-    foreach ( $summary as $post_data ) {
+    foreach ($summary as $post_data) {
         $message .= 'Title: ' . $post_data['title'] . "\n";
         $message .= 'URL: ' . $post_data['url'] . "\n";
         $message .= 'Meta Title: ' . $post_data['meta_title'] . "\n";
@@ -156,27 +162,32 @@ function send_daily_post_summary_callback() {
         'Content-Type: text/html; charset=UTF-8'
     );
 
-    wp_mail( $to , $subject, $message , $headers);
-    
+    wp_mail($to, $subject, $message, $headers);
 }
 //Google Page Speed
-function get_page_speed_score($url) {
+function get_page_speed_score($url)
+{
 
     $api_key = "416ca0ef-63e4-4caa-a047-ead672ecc874"; // your api key
-	$new_url = "http://www.webpagetest.org/runtest.php?url=".$url."&runs=1&f=xml&k=".$api_key; 
-	$run_result = simplexml_load_file($new_url);
-	$test_id = $run_result->data->testId;
+    $new_url = "http://www.webpagetest.org/runtest.php?url=" . $url . "&runs=1&f=xml&k=" . $api_key;
+    $run_result = simplexml_load_file($new_url);
+    $status = $run_result->statusCode;
+    if ($status == 400) {
+        $error = "API Limit Crossed!";
+        return $error;
+    } else {
+        $test_id = $run_result->data->testId;
 
-    $status_code=100;
-    
-    while( $status_code != 200){
-        sleep(10);
-        $xml_result = "http://www.webpagetest.org/xmlResult/".$test_id."/";
-	    $result = simplexml_load_file($xml_result);
-        $status_code = $result->statusCode;
-        $time = (float) ($result->data->median->firstView->loadTime)/1000;
-    };
+        $status_code = 100;
 
-    return $time;
-    
+        while ($status_code != 200) {
+            sleep(10);
+            $xml_result = "http://www.webpagetest.org/xmlResult/" . $test_id . "/";
+            $result = simplexml_load_file($xml_result);
+            $status_code = $result->statusCode;
+            $time = (float) ($result->data->median->firstView->loadTime) / 1000;
+        };
+
+        return $time;
+    }
 }
